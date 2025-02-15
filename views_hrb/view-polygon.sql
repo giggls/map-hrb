fb8c9029c152bc627954f2b613568790e1eb6820
michael spreng <code@m.spreng.ch>
2025-02-15 10:42:22 +0100

    create indices

--- views_hrb/view-polygon.sql
+++ views_hrb/view-polygon.sql
@@ -51,3 +51,8 @@ SELECT
 FROM planet_osm_polygon;
 
 GRANT select ON view_osmhrb_polygon TO public;
+
+CREATE INDEX view_osmhrb_polygon_admin ON planet_osm_polygon USING GIST (ST_PointOnSurface(way)) WHERE COALESCE(tags->'name:hsb',tags->'name:dsb',name) IS NOT NULL AND boundary = 'administrative' AND admin_level IN ('0', '1', '2', '3', '4');
+CREATE INDEX view_osmhrb_polygon_name ON planet_osm_polygon USING GIST (ST_PointOnSurface(way)) WHERE COALESCE(tags->'name:hsb',tags->'name:dsb',name) IS NOT NULL;
+CREATE INDEX view_osmhrb_polygon_name_z6 ON planet_osm_polygon USING GIST (ST_PointOnSurface(way)) WHERE COALESCE(tags->'name:hsb',tags->'name:dsb',name) IS NOT NULL AND way_area > 5980000;
+
